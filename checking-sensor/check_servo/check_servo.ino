223f955eb29473cb47c0afab369f00a6f8fd7cf1
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2023-06-28 20:00:26 +0700

    chore: commit for checking motor servo code

---
+++ checking-sensor/check_servo/check_servo.ino
@@ -0,0 +1,24 @@
+#include <Servo.h> //library servo
+
+Servo myservo; // membuat variabel servo untuk dikendalikan
+int pos = 0; // deklarasi variabel untuk posisi sudut
+
+void setup()
+{
+  myservo.attach(9); //deklarasi servo pada pin 9
+}
+
+void loop()
+{
+  for (pos = 0; pos < 180; pos += 1) // perulangan untuk posisi 0 sampai 180 derajat
+  { // step setiap 1 derajat
+    myservo.write(pos); // memerintahkan servo ke posisi derajat sesuai nilai variabel pos
+    delay(5); // menunggu 15 milidetik
+  }
+
+  for (pos = 90; pos >= 1; pos -= 1) // perulangan untuk posisi 180 sampai 0 derajat
+  {
+    myservo.write(pos); // memerintahkan servo ke posisi derajat sesuai nilai variabel pos
+    delay(5); // menunggu 15 milidetik
+  }
+}
