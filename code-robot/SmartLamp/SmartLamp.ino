b91d5b2a8b42b0ceb96e03fe3b324a53f91a3ef3
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2023-06-28 19:52:08 +0700

    chore: commit SmartLamp code

---
+++ code-robot/SmartLamp/SmartLamp.ino
@@ -0,0 +1,33 @@
+//Codingan Arduino - Smart Lamp (Clapping Lamp)
+//Robonesia - Latih Hobi
+//Kampung Padi (Dago Pojok) B11
+
+const int relay = 13;
+const int sensor = 2;
+boolean relayAktif = false;
+int keadaansebelumnya = LOW;
+
+void setup() {
+  pinMode(relay, OUTPUT);
+  pinMode(sensor, INPUT);
+}
+
+void loop() {
+  int keadaansekarang = digitalRead(sensor);
+  if ((keadaansekarang == HIGH) and (keadaansekarang != keadaansebelumnya)) {
+    //  Membalik keadaan relay aktif
+    relayAktif = not relayAktif;
+
+    //Atur relay berdasarkan nilai relayAktif
+
+    if (relayAktif) {
+      digitalWrite(relay, HIGH);
+      delay(100);  //tunda sesaat
+    } else {
+      digitalWrite(relay, LOW);
+      delay(100);  //tunda sesaat
+    }
+  }
+  //perbaharui keadaan sebelumnya
+  keadaansebelumnya = keadaansekarang;
+}
