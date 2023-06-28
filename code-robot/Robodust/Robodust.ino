3e997e11bfb482459610c036f87cee1b4e19eb56
Yogi Hendrawan <hendrawanyogi79@gmail.com>
2023-06-28 19:50:20 +0700

    chore: commit Robodust Code

---
+++ code-robot/Robodust/Robodust.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+const int trigPin = 2;
+const int echoPin = 3;
+Servo myservo;
+long duration;
+int distance;
+int previousDistance = 0;
+
+void setup() {
+  myservo.attach(9);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  int distance = duration * 0.034 / 2;
+  Serial.print("Jarak= ");
+  Serial.println(distance);
+
+  if (distance < 10 && previousDistance >= 10) {
+    myservo.write(75);
+    delay(3000);
+    myservo.write(0);
+  }
+
+  previousDistance = distance;
+
+  delay(200);
+}
